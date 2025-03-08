189b884746682b877a0d55773f0659c36282ed61
Jason Juhyun Kim <mtinet@hanmail.net>
2025-03-09 00:55:45 +0900

    Update shorten.js

--- netlify/functions/shorten.js
+++ netlify/functions/shorten.js
@@ -6,6 +6,7 @@ if (!admin.apps.length) {
         admin.initializeApp({
             credential: admin.credential.cert(serviceAccount)
         });
+        console.log("✅ Firebase successfully initialized.");
     } catch (error) {
         console.error("🔥 Firebase Initialization Error:", error);
     }
